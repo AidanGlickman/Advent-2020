78d860725f91b7d03d3c8bae7ec7b3dbaeafe1f6
Aidan Glickman <aidan@aidanglickman.com>
2020-12-01 16:53:18 -0500

    add day 1

---
+++ day1/solution.py
@@ -0,0 +1,32 @@
+from os import read
+
+
+def readEntries(filename="input.txt"):
+    with open(filename, "r") as filein:
+        return [int(line.rstrip()) for line in filein]
+
+
+def part1(entries):
+    entriesset = set(entries)
+    if 1010 in entries:
+        return 1010 ** 2
+    for i in entries:
+        if (2020 - i) in entriesset:
+            return f"{i} * {2020 - i} = {i * (2020  - i)}"
+
+
+def part2(entries):
+    entriesset = set(entries)
+    for x, i in enumerate(entries[:-1]):
+        for j in entries[x:]:
+            if (2020 - (i + j)) in entriesset:
+                return f"{i} * {j} * {2020 - (i + j)} = {i * j * (2020 - (i + j))}"
+
+
+def main():
+    entries = readEntries()
+    print(part1(entries))
+    print(part2(entries))
+
+
+main()
